829cbfd37f1d7316f075d918e2d66731f59011ec
Ana Carolina <carol.oliva.cruz@gmail.com>
2024-03-12 11:06:46 -0300

    style: update style global

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo soluta
+        exercitationem ex, alias illum inventore facere vitae consequuntur
+        corrupti maxime accusantium quia qui suscipit recusandae sit ipsum eius
+        sed. Amet!
+      </p>
+    </div>
+  );
 }
